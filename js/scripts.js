050cf78cc32bf01586f97d2b5d594e258103eefd
taylulz <taylulzcode@gmail.com>
2020-10-30 14:59:08 -0700

    modify first regex from + to ? It now passes test spec 7

--- js/scripts.js
+++ js/scripts.js
@@ -11,23 +11,20 @@ $(document).ready(function() {
     }
     
     let stringArray = displayArray.toString();
-    let finalString1 = stringArray.replace(/.3+../g, " Won't you be my cyber neighbor?, ");
+    let finalString1 = stringArray.replace(/.3..?/g, " Won't you be my cyber neighbor?, ");
     let finalString2 = finalString1.replace(/.2..?/g, " Boop!, ");
+    let finalString3 = finalString2.replace(/.1..?/g, " Beep!, ")
 
     // let finalString2 = finalString1.replace(/[2]/g, " Boop!");
     // let finalString3 = finalString2.replace(/[1]/g, " Beep!")
     // let finalString4 = finalString3.replace(/Boop\!\ Beep\!\ /g, " Boop!")
     
     
-    $("#result").text(finalString2);
+    $("#result").text(finalString3);
     
     e.preventDefault();
   });
 });
-
-
-// 6 Test: "It should return a "Boop!" when 21 is inputted
-// Expect(mrRobo(21)).toEqual("Boop!");
 
 // 7 Test: "It should return a "Boop!" when 32 is inputted
 // Expect(mrRobo(32)).toEqual("Won't you be my cyber Neighbor?");
