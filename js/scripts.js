3e2f41570d5404fff474860f6e8307d5785c8ff6
taylulz <taylulzcode@gmail.com>
2020-10-30 13:19:10 -0700

    replace previous results with new results

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,22 @@ $(document).ready(function() {
   $("#numberForm").submit(function (e) {
     e.preventDefault();
 
+  
+
     let countToInt = $("input#inputNumber").val()
     let displayArray = []
     
     for (let i = 0; i <= countToInt; i ++) {
-      displayArray.push(i + " ");
+      displayArray.push(" " + i);
     }
 
     let stringArray = displayArray.toString();
-    let finalString1 = stringArray.replace(/[1]/g, "Beep!");
-    let finalString2 = finalString1.replace(/[2]/g, "Boop!");
+    let finalString1 = stringArray.replace(/[.1]/g, " Beep!");
+    let finalString2 = finalString1.replace(/[2.]/g, " Boop!");
+    let finalString3 = finalString2.replace(/[.3]/g, " Won't you be my cyber neighbor?")
 
     
-    $("#result").append(finalString2);
+    $("#result").text(finalString3);
 
   });
 });
