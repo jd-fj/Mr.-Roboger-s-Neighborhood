91cd50edb5a92caa0eb2ac1f0968ce99173c7e59
taylulz <taylulzcode@gmail.com>
2020-11-01 08:42:51 -0800

    change displayArray to humanArray, to keep mrRobo's counting and normal human counting different

--- js/scripts.js
+++ js/scripts.js
@@ -3,33 +3,38 @@ $(document).ready(function() {
     e.preventDefault();
 
 let countToInt = $("input#inputNumber").val()
-const beep = "Beep!, "
-const boop = "Boop!, "
-const neighb = "3"
-let displayArray = []
+const beep = 
+const boop = 
+const neighbor = 
+let humanArray = []
 
+// okay, so here I have a loop, that makes an array of stringy numbers, counting up to the number the user inputs. 
 for (let i = 0; i <= countToInt; i ++) {
-  if (neighb.includes(i)) {
-    displayArray.push("Won't you be my neighb")
-  }
-  // if else (i.includes("2")) {
-  //   displayArray.push("Boop!, ")
-  // }
-  // if else (i.includes("1")) {
-  //   displayArray.push("Beep!, ")
-  // }
-  else displayArray.push(" " + i);
+  humanArray.push(" " + i);
 }
 
-    // let stringArray = displayArray.toString();
-    // let finalString1 = stringArray.replace(/.3..?/g, " Won't you be my cyber neighbor?, ");
-    // let finalString2 = finalString1.replace(/.2..?/g, " Boop!, ");
-    // let finalString3 = finalString2.replace(/.1..?/g, " Beep!, ")
+// Now what I want to do it loop through that array, and replace all the threes with "Wybmn?"
+
 
     $("#result").text(displayArray);    
   });
 });
 
+
+// if (neighbor.includes(i)) {
+//   displayArray.push("Won't you be my neighb")
+// }
+// if else (i.includes("2")) {
+//   displayArray.push("Boop!, ")
+// }
+// if else (i.includes("1")) {
+//   displayArray.push("Beep!, ")
+// }
+
+  // let stringArray = displayArray.toString();
+  // let finalString1 = stringArray.replace(/.3..?/g, " Won't you be my cyber neighbor?, ");
+  // let finalString2 = finalString1.replace(/.2..?/g, " Boop!, ");
+  // let finalString3 = finalString2.replace(/.1..?/g, " Beep!, ")
 
 // let countToInt = $("input#inputNumber").val()
 // let displayArray = []
