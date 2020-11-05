2dd6cb661bb9e0388c5f9d38c1088c6a358b4b10
taylulz <taylulzcode@gmail.com>
2020-11-04 20:42:00 -0800

    save checkpoint

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,35 @@
+
+const beep = " Beep!"
+const boop = " Boop!"
+const neighbor = " Won't you be my neighbor?"
+let humanArray = []
+let roboArray = []
+
+let mrHuman = function() {
+  for (let i = 0; i <= countToInt; i ++) {
+  humanArray.push(" " + i);
+  }
+}
+
+let mrRobo = function(countToInt) {
+  humanArray.forEach(function(humanNumber) {
+    if (humanNumber.includes(3)) {
+      roboArray.push(neighbor);
+    } else if (humanNumber.includes(2)) {
+      roboArray.push(boop);
+    } else if (humanNumber.includes(1)) {
+      roboArray.push(beep);
+    } else { 
+      roboArray.push(humanNumber);
+    };
+  });
+  return roboArray;
+}
+    
 $(document).ready(function() {
   $("#numberForm").submit(function (e) {
     e.preventDefault();
-
-    let countToInt = $("input#inputNumber").val()
-    const beep = " Beep!"
-    const boop = " Boop!"
-    const neighbor = " Won't you be my neighbor?"
-    let humanArray = []
-    let roboArray = []
-
-    for (let i = 0; i <= countToInt; i ++) {
-      humanArray.push(" " + i);
-    }
-
-    let mrRobo = humanArray.forEach(function(humanNumber) {
-      if (humanNumber.includes(3)) {
-        roboArray.push(neighbor);
-        } else if (humanNumber.includes(2)) {
-          roboArray.push(boop);
-        } else if (humanNumber.includes(1)) {
-          roboArray.push(beep);
-        } else { 
-          roboArray.push(humanNumber);
-        };
-      });
-
+    let countToInt = $("input#inputNumber").val();
     $("#result").text(roboArray);  
   });
 });
