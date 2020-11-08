aeae3633583535f96e7f6fc79d727898001f21d3
taylulz <taylulzcode@gmail.com>
2020-11-08 13:45:07 -0800

    restore correct version of repo

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,32 @@
+function mrRobo(userNumber) {
+  const beep = " Beep!"
+  const boop = " Boop!"
+  const neighbor = " Won't you be my neighbor?"
+  let humanArray = []
+  let roboArray = []
+  for (let i = 0; i <= userNumber; i ++) {
+    humanArray.push(" " + i);
+  }
+
+  humanArray.forEach(function(humanNumber) {
+    if (humanNumber.includes(3)) {
+      roboArray.push(neighbor);
+    } else if (humanNumber.includes(2)) {
+      roboArray.push(boop);
+    } else if (humanNumber.includes(1)) {
+      roboArray.push(beep);
+    } else {
+      roboArray.push(humanNumber);
+    }
+  });
+  return roboArray;
+  }
+
 $(document).ready(function() {
   $("#numberForm").submit(function (e) {
     e.preventDefault();
-
-    let countToInt = $("input#inputNumber").val()
-    const beep = " Beep!"
-    const boop = " Boop!"
-    const neighbor = " Won't you be my neighbor?"
-    let humanArray = []
-    let roboArray = []
-
-    for (let i = 0; i <= countToInt; i ++) {
-      humanArray.push(" " + i);
-    }
-
-    let mrRobo = humanArray.forEach(function(humanNumber) {
-      if (humanNumber.includes(3)) {
-        roboArray.push(neighbor);
-        } else if (humanNumber.includes(2)) {
-          roboArray.push(boop);
-        } else if (humanNumber.includes(1)) {
-          roboArray.push(beep);
-        } else { 
-          roboArray.push(humanNumber);
-        };
-      });
-
-    $("#result").text(roboArray);  
+    let countToInt = $("input#inputNumber").val();
+    let finalArray = mrRobo(countToInt);
+    $("#result").text(finalArray);
   });
-});
+}); 
